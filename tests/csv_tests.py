c6a3b3c43e03710c17f6cb914d2c01d46d8b9acb
Alyssa Grace <92542067+Alyssa4854@users.noreply.github.com>
2023-10-11 06:12:52 -0400

    test stubs for csv

--- tests/csv_tests.py
+++ tests/csv_tests.py
@@ -1,2 +1,17 @@
 import unittest
 
+# Alyssa Maguire
+
+# unittests for Issue 17 upload CSV
+# https://github.com/VERSO-UVM/interactive-management-app/issues/17
+
+class TestCSVMethods(unittest.TestCase):
+
+    def test_open(self):
+        pass
+
+    def test_missing_file(self):
+        pass
+
+    def test_file_lock(self):
+        pass
