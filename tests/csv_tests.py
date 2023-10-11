628282fea589198938d56afb2d2933427311a00b
Alyssa Grace <92542067+Alyssa4854@users.noreply.github.com>
2023-10-11 06:39:04 -0400

    patch input to test csv open

--- tests/csv_tests.py
+++ tests/csv_tests.py
@@ -1,11 +1,20 @@
-import unittest
+import sys
+import os.path
+
+from unittest.mock import patch
+from unittest import TestCase
+from unittest import main as test
+
 
 # Alyssa Maguire
 
 # unittests for Issue 17 upload CSV
 # https://github.com/VERSO-UVM/interactive-management-app/issues/17
 
-class TestCSVMethods(unittest.TestCase):
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from Issues.Issue17_13 import csvImport
+class TestCSVMethods(TestCase):
     """
     Alyssa Maguire
 
@@ -14,11 +23,18 @@ class TestCSVMethods(unittest.TestCase):
     Modify test data file paths if necessary
     """
 
-    def test_open(self):
-        pass
+    __TEST_DATA_FILE: str = "test_data/set1.csv"
+
+    @patch('Issues.Issue17_13.csvImport', return_value=__TEST_DATA_FILE)
+    def test_open(self, input):
+        self.assertEqual(csvImport(), True)
 
     def test_missing_file(self):
         pass
 
     def test_file_lock(self):
         pass
+
+
+if __name__ == '__main__':
+    test()
