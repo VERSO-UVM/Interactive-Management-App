060a3a9218d66cc9a039469c3943b61475e9f697
Grace Kinney <112431548+gracekinney@users.noreply.github.com>
2023-10-14 12:02:34 -0400

    changed name of file, added code to sort data (column type and order) based on user input

---
+++ Issues/Issue16.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+#read dataframe (using testing csv)
+df = pd.read_csv('test_data/set3.csv')
+
+#setting pandas to display full length of data in dataframe
+#for user to view
+pd.set_option('display.max_colwidth', None)
+
+
+#number of data values
+len_of_df = len(df)
+#create number of headers
+len_of_Headers = len(list(df.columns))
+#create list of column names
+column_Names = list(df.columns)
+
+#print number of headers and column names
+print("The dataframe has {} column values: {} and {} lines of data.\n".format(len_of_Headers, column_Names, len_of_df))
+
+select_Column = input("Please select a column to sort by:\n")
+select_Order = input("Would you like to view the data in ascending or descending order?(A or D):")
+
+
+if(select_Column in df.columns):
+    if select_Order.lower() == 'A':
+        sorted_asc = df.sort_values(by=select_Column, ascending=True)
+        print(sorted_asc)
+    elif select_Order.lower() == 'D':
+        sorted_des = df.sort_values(by=select_Column, asending=False)
+        print(sorted_des)
+    else:
+        print("Invalid sorting order.")
+else:
+    print("Invalid column name.")
