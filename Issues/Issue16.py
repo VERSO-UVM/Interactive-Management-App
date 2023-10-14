17a4f7dae407897104ba624b017504e1042aebe4
Grace Kinney <112431548+gracekinney@users.noreply.github.com>
2023-10-14 12:20:27 -0400

    added comments

--- Issues/Issue16.py
+++ Issues/Issue16.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+#funtion name to organize data
 def organize_Data():
     #read dataframe (using testing csv)
     df = pd.read_csv('test_data/set3.csv')
@@ -9,32 +10,38 @@ def organize_Data():
     pd.set_option('display.max_colwidth', None)
 
 
-    #number of data values
+    #number of rows of data
     len_of_df = len(df)
-    #create number of headers
+    #number of column names
     len_of_Headers = len(list(df.columns))
-    #create list of column names
+    #list of column names
     column_Names = list(df.columns)
 
     #print number of headers and column names
     print("The dataframe has {} column values: {} and {} lines of data.\n".format(len_of_Headers, column_Names, len_of_df))
 
+    #user input for sorting
     select_Column = input("Please select a column to sort by:\n")
     select_Order = input("Would you like to view the data in ascending or descending order?(A or D):\n")
 
-
+    #if the user input for column name is in the dataframe, continue
     if(select_Column in df.columns):
+        #if the user input for selected order is ascending
         if select_Order == 'A':
             sorted_asc = df.sort_values(by=select_Column, ascending=True)
             print(sorted_asc)
+        #if the user input for selected order is descending
         elif select_Order == 'D':
             sorted_des = df.sort_values(by=select_Column, ascending=False)
             print(sorted_des)
+        #if the user input for selected order is incorret
         else:
             print("Invalid sorting order.")
+    #if the user input for column name is incorrect
     else:
         print("Invalid column name.")
 
+#testing
 def main():
     organize_Data()
 
