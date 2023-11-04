c4ec39ffea6ef6dc0e7d304a23c6e3c91e75b085
Alyssa Grace <92542067+Alyssa4854@users.noreply.github.com>
2023-11-04 00:18:52 -0400

    function prototype

--- Issues/select_factors.py
+++ Issues/select_factors.py
@@ -9,4 +9,14 @@ from typing import List as _List
 
 
 def select_factors(factors: _List[str]) -> _List[str]:
-    pass
+
+    option: str = '0'
+    keepers: _List[str] = []
+    for i in factors:
+        print(i)
+        while not (option == 'y' or option == 'n'):
+            option = input("Keep? y/n: ").lower()
+        if option == 'y':
+            keepers.append(i)
+    
+    return keepers
