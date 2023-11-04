e72313dcdce3a73141ac053cd62665766396f12e
Alyssa Grace <92542067+Alyssa4854@users.noreply.github.com>
2023-11-04 00:13:29 -0400

    select factors stub

---
+++ Issues/select_factors.py
@@ -0,0 +1,11 @@
+## @author alyssa   # noqa: E266
+
+"""
+Select from a list of factors
+"""
+
+from typing import List as _List
+
+
+def select_factors(factors: _List[str]) -> _List[str]:
+    pass
