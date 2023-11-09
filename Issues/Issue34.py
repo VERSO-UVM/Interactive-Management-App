72a8731caab02521ae27fe7696667cbc22a00f6f
Grace Kinney <112431548+gracekinney@users.noreply.github.com>
2023-11-08 21:33:51 -0500

    created idea voting class: default values, custom constructors, getters, setters

--- Issues/Issue34.py
+++ Issues/Issue34.py
@@ -1,1 +1,43 @@
-print("Hello World!")
+##Idea Voting Class
+class IdeaVoting:
+    # default values
+    title = "None"
+    category = "None"
+    numVotes = 0
+    percentVotes = 0
+
+    # OR DO WE WANT A DEFAULT CONSTRUCTOR
+
+    # custom constructor for IdeaVoting class
+    def __init__(self, title, category, numVotes, percentVotes):
+        # setting private fields
+        self.__title = title
+        self.__category = category
+        self.__numVotes = numVotes
+        self.__percentVotes = percentVotes
+    
+    # getters
+    def getTitle(self):
+        return self.__title
+    
+    def getCategory(self):
+        return self.__category
+    
+    def getNumVotes(self):
+        return self.__numVotes
+    
+    def getPercentVotes(self):
+        return self.__percentVotes
+    
+    # setters
+    def setTitle(self, newTitle):
+        self.__title = newTitle
+
+    def setCategory(self, newCat):
+        self.__category = newCat
+
+    def setNumVotes(self, newNumVotes):
+        self.__numVotes = newNumVotes
+
+    def setPercentVotes(self, newPercentVotes):
+        self.__percentVotes = newPercentVotes
