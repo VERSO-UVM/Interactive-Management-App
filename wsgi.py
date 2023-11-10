345b1c70be57028fc9b4af00a4fc226298b1e351
Alyssa Grace <92542067+Alyssa4854@users.noreply.github.com>
2023-11-09 23:03:38 -0500

    update remote wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,32 @@
 import argparse
 from app import app
 
-## @author Alyssa
+# This must be True on the compute engine. It should be false for development.
+REMOTE_SERVER = False
+
+
+# @author Alyssa
 # These are the CLI arguments that the ISM application accepts.
 # Example:
 # python wsgi.py --port=5001 --host=localhost --debug
-parser = argparse.ArgumentParser(description='Start the ORCA ISM application')
-parser.add_argument('--debug', dest='debug', action='store_const', const=True, default=False, help='Start in debug mode')
-parser.add_argument('--port', dest='port', action='store', type=int, default=5000, help='Specify host port')
-parser.add_argument('--host', dest='host', action='store', type=str, default='0.0.0.0', help='Specify host address')
+def parse_it():
+    parser = argparse.ArgumentParser(
+        description='Start the ORCA ISM application')
+    parser.add_argument('--debug', dest='debug', action='store_const',
+                        const=True, default=False, help='Start in debug mode')
+    parser.add_argument('--port', dest='port', action='store',
+                        type=int, default=5000, help='Specify host port')
+    parser.add_argument('--host', dest='host', action='store',
+                        type=str, default='0.0.0.0', help='Specify host address')
 
-args = parser.parse_args()
+    return parser.parse_args()
 
-if __name__ == "__main__":
+
+if __name__ == "__main__" and not REMOTE_SERVER:
+    args = parse_it()
     app.run(debug=args.debug,
             port=args.port,
             host=args.host)
+
+elif __name__ == "__main__":
+    app.run(host="0.0.0.0")
