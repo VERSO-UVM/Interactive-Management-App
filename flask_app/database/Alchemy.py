ce5e5a3e51c856a19f69e5503996fbfa01c2fe13
Alyssa Grace <92542067+Alyssa4854@users.noreply.github.com>
2023-11-29 18:40:25 -0500

    made db connector

--- flask_app/database/Alchemy.py
+++ flask_app/database/Alchemy.py
@@ -37,3 +37,22 @@ class Participant(Base):
 
     def __repr__(self):  # string representation
         return f'{self.username} => name: {self.first_name} {self.last_name}, email: {self.email}'
+
+
+def initialize_database_connection() -> Session:
+    """
+    Initializes a connection to the database
+    :return: The active session to the database
+    :rtype: Session
+    """
+
+    DATABASE_LOCATION: STR = "/database/data.sqlite3"
+    # Connects to the database file: toggle echo to see activity in console
+    engine = create_engine("sqlite://" + DATABASE_LOCATION, echo=False)
+
+    # Creates all the classes from above in the database
+    Base.metadata.create_all(bind=engine)
+
+    _Session = sessionmaker(bind=engine)
+
+    return _Session()
