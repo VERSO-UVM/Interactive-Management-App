b60007744befe09b6ff52565259d79f22f5d0ee0
Alyssa Grace <92542067+Alyssa4854@users.noreply.github.com>
2023-12-06 04:01:11 -0500

    new database tables

--- flask_app/database/Alchemy.py
+++ flask_app/database/Alchemy.py
@@ -40,7 +40,18 @@ class Participant(Base):
 
     def __repr__(self):  # string representation
         return f'{self.username} => name: {self.first_name} {self.last_name}, email: {self.email}'
-    
+
+
+class Category(Base):
+
+    __tablename__ = 'categories'
+
+    id = Column('id', String, primary_key=True)
+    name = Column('name', String, nullable=False)
+    t_created = Column('created_at', TIMESTAMP, nullable=False)
+    t_updated = Column('last_updated', TIMESTAMP, nullable=True)
+
+
 class Idea(Base):
 
     __tablename__ = 'ideas'
@@ -51,19 +62,19 @@ class Idea(Base):
     def __init__(self,
                  title: str,
                  votes: int):
-        
+
         self.title = title
         self.votes = votes
-    
+
     def __repr__(self):
         return f'{self.title}: {self.votes} votes'
-    
+
 
 class Factor(Base):
 
     __tablename__ = 'factors'
 
-    id = Column('id', Integer, primary_key=True)
+    id = Column('id', String, primary_key=True)
     idea = Column('parent_idea', String, nullable=False)
     t_created = Column('created_at', TIMESTAMP, nullable=False)
     description = Column('description', String, nullable=True)
@@ -71,15 +82,22 @@ class Factor(Base):
     t_updated = Column('last_updated', TIMESTAMP, nullable=True)
 
     def __init__(self,
-                 id: int,
+                 id: str,
                  idea: str,
                  t_created: datetime,
                  description: str = None,
                  label: str = None,
                  t_updated: datetime = None):
 
+        self.id = id
+        self.idea = idea
+        self.t_created = t_created
+        self.description = description
+        self.label = label
+        self.t_updated = t_updated
 
-
+    def __repr__(self):
+        return f'Factor #{self.id} "{self.label}" >> Idea = {self.idea} >> created at {self.t_created}'
 
 
 def initialize_database_connection() -> Session:
