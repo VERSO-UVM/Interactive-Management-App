22235f8027715fdb9ca4689cc81ee0c5b96b8fae
Alyssa Grace <92542067+Alyssa4854@users.noreply.github.com>
2023-12-06 04:29:10 -0500

    organized lib, removed duplicates

--- flask_app/lib/dTypes/Participant.py
+++ flask_app/lib/dTypes/Participant.py
@@ -34,25 +34,3 @@ class Participant:
 
     def __repr__(self):
         return self.name + self.willVote + self.haveVoted + ""
-
-
-# Class for an idea
-class Idea:
-    name = "None"
-    purpose = "Main Purpose"
-
-    def __init__(self, name, purpose):
-        self.name = name
-        self.purpose = purpose
-
-    def chnageName(self, name):
-        self.name = name
-
-    def chnagePurpose(self, purpose):
-        self.purpose = purpose
-
-    def getPurpose(self):
-        return self.purpose
-
-    def getName(self):
-        return self.name
